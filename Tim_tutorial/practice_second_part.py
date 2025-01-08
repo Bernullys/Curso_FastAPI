365e34141b0cdeedf80189c3f6aa131938c0f2c0
badr <bernardoantoniod@gmail.com>
2025-01-08 12:05:32 -0300

    puting and deleting

--- Tim_tutorial/practice_second_part.py
+++ Tim_tutorial/practice_second_part.py
@@ -10,6 +10,7 @@ class Person(BaseModel):
     size: float
 
 # This will be a dict of instances of class Person
+# This should be a database
 persons_intances = {}
 
 # Basic way of posting info:
@@ -26,9 +27,27 @@ def add_details(person_name: str, person: Person):
     persons_intances[person_name] = person
     return persons_intances[person_name]
 
-# This is an endpoint to get the persons I'm posting before (which will be storing on dict persons_instances)
+# This is an endpoint to get (check) the persons I'm posting before (which will be storing on dict persons_instances)
 @application.get("/check_person_posted")
-def check_person(name):
+def check_person(nick_name):
     for person in persons_intances:
-        if persons_intances[person].nick_name == name:
+        if persons_intances[person].nick_name == nick_name:
             return persons_intances[person]
+
+# This is and endpoint to update the details of a person instance
+@application.put("/updating/{person_name}")
+def updating_person(person_name: str, person: Person):
+    if person.nick_name != None:
+        persons_intances[person_name].nick_name = person.nick_name
+    if person.age != None:
+        persons_intances[person_name].age = person.age
+    if person.size != None:
+        persons_intances[person_name].size = person.size
+
+    return persons_intances[person_name]
+
+# This endpoint is to delete a person instance
+@application.delete("/deleting/{person_name}")
+def deleting_person(person_name: str):
+    del persons_intances[person_name]
+    return {"Success": "Person deleted!"}
